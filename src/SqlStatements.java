dff9e8e6481214551bb83caba1128215123a4468
Kevindrayana <kistimur@connect.hku.hk>
2025-03-15 21:59:06 +0800

    minor update

--- src/SqlStatements.java
+++ src/SqlStatements.java
@@ -5,10 +5,10 @@ public class SqlStatements {
     public static final String INSERT_USER_TO_GAME = "INSERT INTO user_to_game (user_id, game_id) VALUES (?, ?)";
     public static final String READ_USERS = "SELECT name, password FROM users WHERE name = ?";
     public static final String READ_ONLINE_USERS = "SELECT name FROM online_users WHERE name = ?";
-    public static final String READ_GAMES = "SELECT name FROM online_users WHERE name = ?";
-    public static final String READ_USER_TO_GAME = "SELECT name FROM online_users WHERE name = ?";
+    // public static final String READ_GAMES = "SELECT name FROM online_users WHERE name = ?";
+    // public static final String READ_USER_TO_GAME = "SELECT name FROM online_users WHERE name = ?";
 
-    public static final String UPDATE_USERS = "";
+    // public static final String UPDATE_USERS = "";
     public static final String REFRESH_ONLINE_USERS = "TRUNCATE TABLE online_users";
 
     public static final String DELETE_USERS = "";
